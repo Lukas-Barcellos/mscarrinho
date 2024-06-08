dfebcb1a774e293f2b688c5cdf18cc215e9a08f0
Lukas Barcellos <lknasbar@hotmail.com>
2024-06-08 11:51:13 -0300

    primeiro start

---
+++ src/main/java/carrinho/fiap/mscarrinho/MscarrinhoApplication.java
@@ -0,0 +1,13 @@
+package carrinho.fiap.mscarrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscarrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscarrinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carrinho/fiap/mscarrinho/MscarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package carrinho.fiap.mscarrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
